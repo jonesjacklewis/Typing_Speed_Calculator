fcee3d22c63f7220ef897b2c678a4aa4cd69e6af
Jack Jones <44882194+jonesjacklewis@users.noreply.github.com>
2020-10-18 11:02:53 +0100

    Update TypingSpeedTest.py

--- TypingSpeedTest.py
+++ TypingSpeedTest.py
@@ -5,6 +5,7 @@ pointsNeeded = len(testSentence)  # The number of points available is equal to t
 weight = 1 / pointsNeeded  # The weight of equal char is equal to the reciprocal of the total points
 print("Please type the following sentence as quickly an accurately as possible. For every error your time will be "
       "increased by a calculated percentage ")
+print(testSentence)
 print("\n")
 print("Press a key when ready: ")
 input()
@@ -14,6 +15,18 @@ start = time()  # Starts Timer by saving the current time
 user = input()  # When user press Enter
 stop = time()  # Ends timer by saving the new time
 NumErrors = 0  # Variable for number of errors initially set to zero
+
+
+# Below fixes length issues
+if len(user) > len(testSentence):
+    while len(user) != len(testSentence):
+        testSentence += "*"
+
+if len(user) < len(testSentence):
+    while len(user) != len(testSentence):
+        user += "*"
+
+
 for i in range(pointsNeeded - 1):  # For each character in test string
     if user[i] != testSentence[i]:  # If the character doesn't match what should
         NumErrors += 1  # Increments number of errors
@@ -21,9 +34,16 @@ UserWeight = NumErrors * weight  # Works out an adjustment
 originalTime = stop - start  # Calculates time without errors
 adjustTime = originalTime * (1 + UserWeight)  # Calculates an adjusted time based on errors
 
-originalTime = str(round(originalTime, 2))  # Converts to string and rounds
-NumErrors = str(NumErrors)  # Converts to String
-newTime = str(round(adjustTime, 2))  # Converts to string and rounds
+originalTime = round(originalTime, 2)  #  rounds
+NumErrors = NumErrors
+newTime = round(adjustTime, 2)  # rounds
 
-print("You would have completed the test in %s seconds but due to %s errors your adjusted time is %s seconds" %
-      (originalTime, NumErrors, adjustTime))
+numWords = len(user.replace(".","").split(" ")) # Gets number of words
+sToM = round(newTime / 60, 2)  # Converts to minutes and rounds
+wpm = round(numWords/sToM, 2)  # Calculates wpm and rounds
+
+
+
+
+print(f"You would have completed the test in {originalTime} seconds but due to {NumErrors} error(s) your adjusted time is {adjustTime} seconds")
+print(f"This means your wpm is {wpm}")
